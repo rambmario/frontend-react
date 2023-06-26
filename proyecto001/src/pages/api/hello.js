50a64ca702ebfeba7d576a3dc1635f1edb2cb5cd
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2023-06-26 20:29:06 -0300

    latest version with chatbot

--- proyecto001/src/pages/api/hello.js
+++ proyecto001/src/pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
+  // me conecto a la base de datos mysql pero antes
+  // saco informacion del request.
   res.status(200).json({ name: 'John Doe' })
 }
